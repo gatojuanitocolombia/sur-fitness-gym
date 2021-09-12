e4d01dedb07b6c95499b14865d4c3b3d5ba6b282
Ubuntu <barcasnerd@web-backend.5pdbwsyftyrejfmcukbm533knc.bx.internal.cloudapp.net>
2021-09-12 22:15:59 +0000

    feat: create landing page template

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+path = require('path');
+morgan = require('morgan');
+
+const app = express();
+
+// manejo de las rutas
+const landingPage = require('./routes/LandingPage');
+
+
+// configuraciones de puerto
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// middlewares
+
+
+// rutas de entrada
+app.use('/', landingPage);
+
+
+
+// indicando los archivos publicos del lado del cliente
+app.use("/public",express.static(path.join(__dirname,'public')))
+
+// inicializando el servidor
+app.listen(app.get('port'), () => {
+  console.log(`server on port ${app.get('port')}`);
+});
